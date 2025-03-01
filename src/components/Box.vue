b9c007ce09edc33a4967156d40524fdf5349a2fc
cleiton <cleitonmpc91@gmail.com>
2025-03-01 17:11:43 -0300

    commit-inicial

---
+++ src/components/Box.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="box has-text-wight-bold">
+    <slot></slot>
+    </div>
+</template>
+
+<script lang="js">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'BoxTask'
+})
+</script>
+
+<style scoped>
+.box {
+    background: #faf0ca;
+}
+</style>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,33 @@
+<template>
+    <Box>
+        <div class="columns">
+            <div class="column is-7"> {{ tarefa.descricao || 'Tarefa sem descrição' }} </div>
+            <div class="column">
+                <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+
+
+            </div>
+        </div>
+    </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import Cronometro from './Cronometro.vue';
+import iTarefa from '@/interfaces/ITarefa';
+import Box from './Box.vue';
+
+export default defineComponent({
+    name: "TarefaTask",
+    components: {
+        Cronometro,
+    },
+    props: {
+        tarefa: {
+            type: Object as PropType<iTarefa>,
+            required: true
+        }
+    }
+})
+
+</script>
